388d296e274ecbaa52c598919973e41ceccbd46d
roseckyj <rosecky.jonas@centrum.cz>
2022-09-12 20:16:36 +0200

    Added blueprintjs and i18n

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -1,18 +1,38 @@
-import './App.css';
-import logo from './logo.svg';
+import { Button, NonIdealState, NonIdealStateIconSize, Spinner } from '@blueprintjs/core';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+    const { t, i18n } = useTranslation();
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+        <div
+            style={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <NonIdealState
+                icon={<Spinner size={NonIdealStateIconSize.STANDARD} />}
+                title={t('common:beta.title')}
+                description={t('common:beta.description')}
+                action={
+                    <Button
+                        outlined={true}
+                        text={t('common:beta.toggleLanguage', {
+                            language: i18n.language === 'cs' ? 'en' : 'cs',
+                        })}
+                        icon="flag"
+                        intent="primary"
+                        onClick={() => i18n.changeLanguage(i18n.language === 'cs' ? 'en' : 'cs')}
+                    />
+                }
+            />
         </div>
     );
 }
--- Web/src/index.tsx
+++ Web/src/index.tsx
@@ -1,17 +1,34 @@
+import i18next from 'i18next';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { I18nextProvider } from 'react-i18next';
 import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+
+import common_cs from './translations/cs/common.json';
+import common_en from './translations/en/common.json';
+
+i18next.init({
+    interpolation: { escapeValue: false }, // React already does escaping
+    lng: 'cs',
+    fallbackLng: 'en',
+    resources: {
+        en: {
+            common: common_en,
+        },
+        cs: {
+            common: common_cs,
+        },
+    },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <I18nextProvider i18n={i18next}>
+            <App />
+        </I18nextProvider>
     </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

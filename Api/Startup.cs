747f2008a4f333e2816c4d476a1dfb99a572e262
Adam Štěpánek <xstepan1@fi.muni.cz>
2022-09-19 15:43:15 +0200

    Add Marten

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,10 +1,28 @@
+using Marten;
+using Weasel.Core;
+
 namespace Kafe.Api;
 
 public class Startup
 {
-    public void ConfigureServices(IServiceCollection services)
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services, IHostEnvironment environment)
     {
         services.AddAuthorization();
+        services.AddMarten(options =>
+        {
+            options.Connection(Configuration.GetConnectionString("Marten"));
+            if (environment.IsDevelopment())
+            {
+                options.AutoCreateSchemaObjects = AutoCreate.All;
+            }
+        });
     }
     
     public void Configure(IApplicationBuilder app, IHostEnvironment environment)

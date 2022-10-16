6a0cad0f6b66821c92a94c8c32ed2352bb2711ee
Adam Štěpánek <xstepan1@fi.muni.cz>
2022-10-16 11:38:48 +0200

    Remove the OnConfiguring method from LemmaContext

--- Migrator/Lemma/LemmaContext.cs
+++ Migrator/Lemma/LemmaContext.cs
@@ -47,15 +47,6 @@ public partial class LemmaContext : DbContext
     public virtual DbSet<Vote> Votes { get; set; } = null!;
     public virtual DbSet<WmaProperty> WmaProperties { get; set; } = null!;
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-        {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-            optionsBuilder.UseNpgsql("host=localhost;database=lemma;password=password;username=postgres");
-        }
-    }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Appf>(entity =>

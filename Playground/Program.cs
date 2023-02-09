2d1fd55dd7a39f6c748eae62f272c66f658a6499
Adam Štěpánek <xstepan1@fi.muni.cz>
2023-02-09 01:05:27 +0100

    Update playground

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,17 +1,25 @@
-﻿//using Kafe.Media;
-
-//var samplePath = @"";
-
-//var ffcore = new FFmpegCoreService();
-//var ffcoreTest = await ffcore.GetInfo(samplePath);
-
-//var xabe = new XabeFFmpegService();
-//var xabeTest = await xabe.GetInfo(samplePath);
-
-//Console.WriteLine($"FFMpegCore: {ffcoreTest}");
-//Console.WriteLine($"Xabe.FFmpeg: {xabeTest}");
-
-using Kafe.Ruv;
+﻿using Kafe.Ruv;
+using System.Collections.Immutable;
 
 var ruv = new RuvClient();
 await ruv.LogIn("username", "password");
+
+var timeyFimey = new AddFilmParams(
+    Year: 2022,
+    ArtworkType: RegisterArtworkArtworkType.Actor,
+    IsFestivalWinner: false,
+    NameCS: "Timey FImey",
+    KeywordsCS: ImmutableArray.Create("fi", "cestování časem"),
+    AnnotationCS: "Dva studenti objeví revoluční způsob, jak se učit na Algo.",
+    NameEN: "Timey FImey",
+    KeywordsEN: ImmutableArray.Create("fi", "time travel"),
+    AnnotationEN: "Two students discover a revolutionary new way to study for Algo.",
+    Authors: ImmutableArray.Create(new SaveAuthorParams(
+        PersonalNumber: "personalnumber",
+        FirstName: "Adam",
+        LastName: "Štěpánek",
+        DegreeAfterName: "Bc.")),
+    FestivalDate: new DateTimeOffset(2022, 12, 1, 19, 30, 00, TimeSpan.FromHours(1)),
+    ImagePath: @"logo");
+
+await ruv.AddFilm(timeyFimey);

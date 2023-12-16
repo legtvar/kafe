0c73baa3752d4905ee8f10e9daba5be54b9df8af
Adam Štěpánek <xstepan1@fi.muni.cz>
2023-12-16 19:05:42 +0100

    Add the perms editor to the project group UI

--- Web/src/components/pages/groups/GroupsEdit.tsx
+++ Web/src/components/pages/groups/GroupsEdit.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Heading, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import { Box, Button, Flex, HStack, Heading, Tab, TabList, TabPanel, TabPanels, Tabs, VStack } from '@chakra-ui/react';
 import { t } from 'i18next';
 import { BsX } from 'react-icons/bs';
 import { Link, useParams } from 'react-router-dom';
@@ -7,6 +7,7 @@ import { AwaitAPI } from '../../utils/AwaitAPI';
 import { RightsEditor } from '../../utils/RightsEditor';
 import { Status } from '../../utils/Status';
 import { GroupBasicInfo } from './GroupBasicInfo';
+import { SendAPI } from '../../utils/SendAPI';
 
 interface IGroupsEditProps {}
 
@@ -32,24 +33,59 @@ export function GroupsEdit(props: IGroupsEditProps) {
                     <Tabs>
                         <TabList>
                             <Tab>{t('groupsEdit.tabs.info').toString()}</Tab>
-                            {/* <Tab>{t('groupsEdit.tabs.rights').toString()}</Tab> */}
+                            <Tab>{t('groupsEdit.tabs.rights').toString()}</Tab>
                         </TabList>
 
                         <TabPanels pt={6}>
                             <TabPanel>
                                 <GroupBasicInfo group={group} />
                             </TabPanel>
-                            {/* <TabPanel>
-                                <RightsEditor
-                                    item={group}
-                                    explanation={{
-                                        read: t('rights.groups.group.read').toString(),
-                                        write: t('rights.groups.group.write').toString(),
-                                        inspect: t('rights.groups.group.inspect').toString(),
-                                        append: t('rights.groups.group.append').toString(),
-                                    }}
-                                />
-                            </TabPanel> */}
+                            <TabPanel>
+                                <AwaitAPI request={(api) => api.entities.perms.getById(group.id)}>
+                                    {(perms) => (
+                                        <VStack align="stretch">
+                                            <SendAPI
+                                                value={perms}
+                                                request={(api, value) => api.entities.perms.update(value)}
+                                                onSubmited={() => {}}
+                                                repeatable={true}
+                                            >
+                                                {(onSubmit, status) => (
+                                                    <HStack align="stretch" justifyContent="flex-end">
+                                                        <Button
+                                                            colorScheme="blue"
+                                                            ml={{
+                                                                base: '0',
+                                                                xl: '4',
+                                                            }}
+                                                            mt={{
+                                                                base: '2',
+                                                                xl: '0',
+                                                            }}
+                                                            onClick={onSubmit}
+                                                            isLoading={status === 'sending'}
+                                                            isDisabled={status === 'sending'}
+                                                        >
+                                                            {t('generic.save').toString()}
+                                                        </Button>
+                                                    </HStack>
+                                                )}
+                                            </SendAPI>
+                                            <RightsEditor
+                                                perms={perms}
+                                                options={['read', 'write', 'inspect', 'append', 'review']}
+                                                explanation={{
+                                                    read: t('rights.groups.group.read').toString(),
+                                                    write: t('rights.groups.group.write').toString(),
+                                                    inspect: t('rights.groups.group.inspect').toString(),
+                                                    append: t('rights.groups.group.append').toString(),
+                                                    review: t('rights.groups.group.review').toString(),
+                                                }}
+                                            />
+                                        </VStack>
+                                    )}
+                                </AwaitAPI>
+                            </TabPanel>
                         </TabPanels>
                     </Tabs>
                 </Box>

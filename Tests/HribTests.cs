de6d87227f71948ab6421f2fe874eaea85e81d60
Adam Štěpánek <xstepan1@fi.muni.cz>
2024-03-30 15:35:55 +0100

    Rename HribTests

--- Tests/HribTests.cs
+++ Tests/HribTests.cs
@@ -5,14 +5,14 @@ namespace Kafe.Tests;
 public class HribTests
 {
     [Fact]
-    public void CastFromStringTest()
+    public void Convert_FromString_ShouldNotThrow()
     {
         Hrib hrib1 = (Hrib)"abcdefghijk";
         Hrib hrib2 = "abcdefghijk";
     }
-    
-        [Fact]
-    public void CastToStringTest()
+
+    [Fact]
+    public void Convert_ToString_ShouldNotThrow()
     {
         string str = (string)Hrib.Create();
     }

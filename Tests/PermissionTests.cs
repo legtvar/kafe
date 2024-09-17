e40216ff0f67a9371dd55784c4bcf0fee0bbb289
Adam Štěpánek <xstepan1@fi.muni.cz>
2024-09-17 20:33:55 +0200

    Add the `EntityPermissionInfo_System_ShouldExist` test

--- Tests/PermissionTests.cs
+++ Tests/PermissionTests.cs
@@ -1,13 +1,23 @@
+using System.Threading;
 using System.Threading.Tasks;
+using Kafe.Data;
+using Kafe.Data.Documents;
+using Kafe.Data.Projections;
 using Xunit;
 
 namespace Kafe.Tests;
 
+[Collection(Const.Collections.Api)]
 public class PermissionTests(ApiFixture fixture) : ApiContext(fixture)
 {
     [Fact]
-    public async Task RoleAssignment_ShouldGiveAccountPermission()
+    public async Task EntityPermissionInfo_System_ShouldExist()
     {
-        
+        using var daemon = await Store.BuildProjectionDaemonAsync();
+        await daemon.RebuildProjectionAsync<EntityPermissionEventProjection>(CancellationToken.None);
+        await using var query = Store.QuerySession();
+        var systemPerms = await query.KafeLoadAsync<EntityPermissionInfo>(Hrib.System);
+        Assert.False(systemPerms.HasErrors);
+        Assert.NotNull(systemPerms.Value);
     }
 }

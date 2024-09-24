e8d7d55d67cca0f6d022e97d146ff80232a311ff
Jonáš Rosecký <rosecky.jonas@gmail.com>
2024-09-24 20:25:02 +0200

    Updated home page

--- Web/src/components/pages/home/Home.tsx
+++ Web/src/components/pages/home/Home.tsx
@@ -5,6 +5,7 @@ import {
     Card,
     CardBody,
     CardFooter,
+    Center,
     Divider,
     Heading,
     HStack,
@@ -17,6 +18,7 @@ import { t } from 'i18next';
 import Countdown from 'react-countdown';
 import { Link } from 'react-router-dom';
 import { useTitle } from '../../../utils/useTitle';
+import { Brand } from '../../brand/Brand';
 import { AwaitAPI } from '../../utils/AwaitAPI';
 import { OutletOrChildren } from '../../utils/OutletOrChildren';
 
@@ -33,65 +35,126 @@ export function Home(props: IHomeProps) {
         </Box>
     );
 
+    const DESCRIPTION_LENGTH = 200;
+
     return (
         <OutletOrChildren>
             <VStack w="full" px={6} py={4} alignItems="stretch">
+                <HStack spacing={6} alignItems="center" mb={6}>
+                    <Center fontSize="6xl" h="auto">
+                        <Brand />
+                    </Center>
+                    <VStack alignItems="start">
+                        <Heading size="2xl" mt={0}>
+                            {t('home.title')}
+                        </Heading>
+                        <Text opacity={0.5}>{t('home.subtitle')}</Text>
+                    </VStack>
+                </HStack>
+                <hr />
+                <Heading size="lg" mb={4} mt={6}>
+                    {t('home.openGroups')}
+                </Heading>
                 <AwaitAPI request={(api) => api.groups.getAll()}>
                     {(groups) => (
-                        <>
-                            <Heading size="lg" mb={4}>
-                                {t('home.openGroups')}
-                            </Heading>
+                        <Box w="full" overflowX="auto" py={4}>
+                            <HStack alignItems="start">
+                                {groups
+                                    .filter((group) => group.isOpen)
+                                    .map((group, i) => (
+                                        <Card w="sm" key={i} flexShrink={0}>
+                                            <CardBody>
+                                                <Stack spacing="3">
+                                                    <Heading size="md">{group.getName()}</Heading>
+                                                    {group.description && (
+                                                        <Box mb={4}>
+                                                            {group.getDescription().substring(0, DESCRIPTION_LENGTH)}
+                                                            {group.getDescription().length > DESCRIPTION_LENGTH &&
+                                                                '...'}
+                                                        </Box>
+                                                    )}
+                                                    {group.deadline && (
+                                                        <Box mb={-6}>
+                                                            <Countdown
+                                                                date={new Date(group.deadline)}
+                                                                renderer={({ days, hours, minutes, seconds }) => (
+                                                                    <>
+                                                                        <Box mb={-5}>
+                                                                            {t('createProject.doNotforget')}
+                                                                        </Box>
+                                                                        <SimpleGrid
+                                                                            columns={4}
+                                                                            display="inline-grid"
+                                                                            w="full"
+                                                                        >
+                                                                            <CountdownItem title={t('countdown.days')}>
+                                                                                {days}
+                                                                            </CountdownItem>
+                                                                            <CountdownItem title={t('countdown.hours')}>
+                                                                                {hours}
+                                                                            </CountdownItem>
+                                                                            <CountdownItem
+                                                                                title={t('countdown.minutes')}
+                                                                            >
+                                                                                {minutes}
+                                                                            </CountdownItem>
+                                                                            <CountdownItem
+                                                                                title={t('countdown.seconds')}
+                                                                            >
+                                                                                {seconds}
+                                                                            </CountdownItem>
+                                                                        </SimpleGrid>
+                                                                    </>
+                                                                )}
+                                                            />
+                                                        </Box>
+                                                    )}
+                                                </Stack>
+                                            </CardBody>
+                                            <Divider />
+                                            <CardFooter>
+                                                <ButtonGroup spacing="2">
+                                                    <Link to={`/auth/groups/${group.id}/create`}>
+                                                        <Button variant="solid" colorScheme="brand">
+                                                            {t('createProject.signUp').toString()}
+                                                        </Button>
+                                                    </Link>
+                                                    <Link to={`/auth/groups/${group.id}/create`}>
+                                                        <Button variant="outline" colorScheme="brand">
+                                                            {t('createProject.details').toString()}
+                                                        </Button>
+                                                    </Link>
+                                                </ButtonGroup>
+                                            </CardFooter>
+                                        </Card>
+                                    ))}
+                            </HStack>
+                        </Box>
+                    )}
+                </AwaitAPI>
+
+                <Heading size="lg" mb={4} mt={10}>
+                    {t('home.myProjects')}
+                </Heading>
+                <AwaitAPI request={(api) => api.projects.getAll()}>
+                    {(projects) => {
+                        return (
                             <Box w="full" overflowX="auto" py={4}>
-                                <HStack>
-                                    {groups
-                                        .filter((group) => group.isOpen)
-                                        .map((group, i) => (
-                                            <Card w="sm" key={i}>
+                                <HStack alignItems="start">
+                                    {projects
+                                        .filter((project) => project.userPermissions.includes('write'))
+                                        .map((project, i) => (
+                                            <Card w="sm" key={i} flexShrink={0}>
                                                 <CardBody>
                                                     <Stack spacing="3">
-                                                        <Heading size="md">{group.getName()}</Heading>
-                                                        {group.description && (
-                                                            <Box mb={4}>{group.getDescription()}</Box>
-                                                        )}
-                                                        {group.deadline && (
-                                                            <Box mb={-6}>
-                                                                <Countdown
-                                                                    date={new Date(group.deadline)}
-                                                                    renderer={({ days, hours, minutes, seconds }) => (
-                                                                        <>
-                                                                            <Box mb={-5}>
-                                                                                {t('createProject.doNotforget')}
-                                                                            </Box>
-                                                                            <SimpleGrid
-                                                                                columns={4}
-                                                                                display="inline-grid"
-                                                                                w="full"
-                                                                            >
-                                                                                <CountdownItem
-                                                                                    title={t('countdown.days')}
-                                                                                >
-                                                                                    {days}
-                                                                                </CountdownItem>
-                                                                                <CountdownItem
-                                                                                    title={t('countdown.hours')}
-                                                                                >
-                                                                                    {hours}
-                                                                                </CountdownItem>
-                                                                                <CountdownItem
-                                                                                    title={t('countdown.minutes')}
-                                                                                >
-                                                                                    {minutes}
-                                                                                </CountdownItem>
-                                                                                <CountdownItem
-                                                                                    title={t('countdown.seconds')}
-                                                                                >
-                                                                                    {seconds}
-                                                                                </CountdownItem>
-                                                                            </SimpleGrid>
-                                                                        </>
-                                                                    )}
-                                                                />
+                                                        <Heading size="md">{project.getName()}</Heading>
+                                                        {project.description && (
+                                                            <Box mb={4}>
+                                                                {project
+                                                                    .getDescription()
+                                                                    .substring(0, DESCRIPTION_LENGTH)}
+                                                                {project.getDescription().length > DESCRIPTION_LENGTH &&
+                                                                    '...'}
                                                             </Box>
                                                         )}
                                                     </Stack>
@@ -99,72 +162,24 @@ export function Home(props: IHomeProps) {
                                                 <Divider />
                                                 <CardFooter>
                                                     <ButtonGroup spacing="2">
-                                                        <Link to={`/auth/groups/${group.id}/create`}>
-                                                            <Button variant="solid" colorScheme="brand">
-                                                                {t('createProject.signUp').toString()}
-                                                            </Button>
-                                                        </Link>
-                                                        <Link to={`/auth/groups/${group.id}/create`}>
-                                                            <Button variant="outline" colorScheme="brand">
-                                                                {t('createProject.details').toString()}
-                                                            </Button>
-                                                        </Link>
-                                                    </ButtonGroup>
-                                                </CardFooter>
-                                            </Card>
-                                        ))}
-                                </HStack>
-                            </Box>
-                        </>
-                    )}
-                </AwaitAPI>
-
-                <AwaitAPI request={(api) => api.projects.getAll()}>
-                    {(projects) => (
-                        <>
-                            <Heading size="lg" mb={4} mt={10}>
-                                {t('home.myProjects')}
-                            </Heading>
-                            <Box w="full" overflowX="auto" py={4}>
-                                <HStack>
-                                    {projects
-                                        .filter(
-                                            (project) =>
-                                                project.userPermissions.includes(
-                                                    'write',
-                                                ) /* TODO: Apply correct permissions */,
-                                        )
-                                        .map((project, i) => (
-                                            <Card w="sm" key={i}>
-                                                <CardBody>
-                                                    <Stack spacing="3">
-                                                        <Heading size="md">{project.getName()}</Heading>
-                                                        {project.description && (
-                                                            <Box mb={4}>{project.getDescription()}</Box>
-                                                        )}
-                                                    </Stack>
-                                                </CardBody>
-                                                <Divider />
-                                                <CardFooter>
-                                                    <ButtonGroup spacing="2">
-                                                        <Link to={`/auth/groups/${project.id}/edit`}>
-                                                            <Button variant="solid" colorScheme="brand">
-                                                                {t('project.edit').toString()}
-                                                            </Button>
-                                                        </Link>
                                                         <Link to={`/auth/groups/${project.id}`}>
-                                                            <Button variant="outline" colorScheme="brand">
+                                                            <Button variant="solid" colorScheme="brand">
                                                                 {t('project.details').toString()}
                                                             </Button>
                                                         </Link>
+                                                        <Link to={`/auth/groups/${project.id}/edit`}>
+                                                            <Button variant="outline" colorScheme="brand">
+                                                                {t('project.edit').toString()}
+                                                            </Button>
+                                                        </Link>
                                                     </ButtonGroup>
                                                 </CardFooter>
                                             </Card>
                                         ))}
                                 </HStack>
                             </Box>
-                        </>
-                    )}
+                        );
+                    }}
                 </AwaitAPI>
             </VStack>
         </OutletOrChildren>

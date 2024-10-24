05eeb27f95288a49a134866f49241cb016b0da3c
Adam Štěpánek <xstepan1@fi.muni.cz>
2024-10-24 17:19:33 +0200

    Change docs colors a little bit and add a favicon

--- Docs/_includes/layouts/base.tsx
+++ Docs/_includes/layouts/base.tsx
@@ -8,9 +8,7 @@ export default ({ title, children }: FrontPageData) => {
     <html>
       <head>
         <meta charSet="utf-8" />
-        <link rel="icon" sizes="32x32" href="/img/favicon-32.png" />
         <link rel="icon" href="/img/favicon.svg" type="img/svg+xml" />
-        <link rel="apple-touch-icon" size={256} href="/img/favicon-256.png" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <meta name="theme-color" content="#ac162c" />
         <title>{title ? `${title} – KAFE` : "KAFE – Kontrol and Archival of Fantastic Entertainment"}</title>

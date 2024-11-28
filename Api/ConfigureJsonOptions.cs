24707913bbccdeaa89df97a77559e369ff6e9f77
Adam Štěpánek <xstepan1@fi.muni.cz>
2024-11-28 13:54:02 +0100

    Add the InitializeImmutableArrayProperties modifier to System.Text.Json's TypeInfoResolver

--- Api/ConfigureJsonOptions.cs
+++ Api/ConfigureJsonOptions.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
@@ -26,5 +31,71 @@ public class ConfigureJsonOptions : IConfigureOptions<JsonOptions>
             ShouldWriteStackTraces = environment.IsDevelopment() || environment.IsStaging()
         });
         o.JsonSerializerOptions.Converters.Add(new ImmutableArrayJsonConverter());
+        o.JsonSerializerOptions.TypeInfoResolver =
+            (o.JsonSerializerOptions.TypeInfoResolver ?? new DefaultJsonTypeInfoResolver())
+            .WithAddedModifier(InitializeImmutableArrayProperties);
+    }
+
+    private static void InitializeImmutableArrayProperties(JsonTypeInfo type)
+    {
+        if (type.Kind == JsonTypeInfoKind.None)
+        {
+            return;
+        }
+
+        var immutableArrayProps = type.Properties.Where(p => p.PropertyType.IsGenericType
+            && p.PropertyType.GetGenericTypeDefinition() == typeof(ImmutableArray<>)
+            && p.Get is not null
+            && p.Set is not null)
+            .ToImmutableArray();
+
+        if (immutableArrayProps.IsEmpty)
+        {
+            return;
+        }
+
+
+        bool IsNullOrDefaultImmutableArray(object? obj, Type arrayType)
+        {
+            if (obj is null)
+            {
+                return true;
+            }
+
+            var isDefaultProp = arrayType
+                .GetProperty(
+                    name: nameof(ImmutableArray<int>.IsDefault),
+                    bindingAttr: BindingFlags.Public | BindingFlags.Instance)
+                ?? throw new NotSupportedException("There is no IsDefault property on ImmutableArray.");
+
+            return (bool)isDefaultProp.GetMethod!.Invoke(obj, null)!;
+        }
+
+        object CreateImmutableArray(Type type)
+        {
+            var genericCreate = typeof(ImmutableArray).GetMethod(
+                name: nameof(ImmutableArray.Create),
+                bindingAttr: BindingFlags.Static | BindingFlags.Public,
+                types: Type.EmptyTypes)
+                    ?? throw new NotSupportedException("There is no static parameterless Create method "
+                        + "on ImmutableArray.");
+            var create = genericCreate.MakeGenericMethod([type]);
+            return create.Invoke(null, null)
+                ?? throw new NotSupportedException($"An instance of ImmutableArray of '{type.Name}' "
+                    + "could not be created.");
+        }
+
+        type.OnDeserialized = o =>
+        {
+            foreach (var immutableArrayProp in immutableArrayProps)
+            {
+                var array = immutableArrayProp.Get!(o);
+                var elementType = immutableArrayProp.PropertyType.GenericTypeArguments.Single();
+                if (IsNullOrDefaultImmutableArray(array, immutableArrayProp.PropertyType))
+                {
+                    immutableArrayProp.Set!(o, CreateImmutableArray(elementType));
+                }
+            }
+        };
     }
 }

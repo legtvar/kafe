1ed792f5c4bd4539348994b13467d7d9e77b240a
Adam Štěpánek <xstepan1@fi.muni.cz>
2025-03-04 14:57:09 +0100

    Add shards to the media mod

--- Media/MediaMod.cs
+++ Media/MediaMod.cs
@@ -4,8 +4,20 @@ namespace Kafe.Media;
 public sealed class MediaMod : IMod
 {
     public const string Name = "media";
-    
+
     public void Configure(ModContext context)
     {
+        context.AddShard<VideoShard>(new()
+        {
+            Name = "video",
+        });
+        context.AddShard<ImageShard>(new()
+        {
+            Name = "image"
+        });
+        context.AddShard<SubtitlesShard>(new()
+        {
+            Name = "subtitles"
+        });
     }
 }

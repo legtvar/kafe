562bed3d61fdaf83a150c11f789f7a01640577de
Adam Štěpánek <xstepan1@fi.muni.cz>
2025-03-04 14:50:50 +0100

    Add BlobShard and ArchiveShard to the core mod

--- Common/KafeTypeUsage.cs
+++ Common/KafeTypeUsage.cs
@@ -2,8 +2,8 @@ namespace Kafe;
 
 public enum KafeTypeUsage
 {
-    None = 0,
-    ArtifactProperty = 1 << 0,
-    ShardMetadata = 1 << 1,
-    Requirement = 1 << 2,
+    None,
+    ArtifactProperty,
+    ShardMetadata,
+    Requirement,
 }

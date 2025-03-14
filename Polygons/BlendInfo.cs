1cabf61c05d671e34acccbc88f5136ceedcf512e
Adam Štěpánek <xstepan1@fi.muni.cz>
2025-03-14 14:54:48 +0100

    Add Kafe.Polygons

--- Polygons/BlendInfo.cs
+++ Polygons/BlendInfo.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 
-namespace Kafe.Media;
+namespace Kafe.Polygons;
 
 public record BlendInfo(
     string FileExtension = Const.BlendFileExtension,

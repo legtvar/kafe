22606ec717d4147fcb2ece5390dd115301651517
Adam Štěpánek <xstepan1@fi.muni.cz>
2025-04-20 21:15:40 +0200

    Add IShard and IArtifact

---
+++ Common/Data/IShard.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace Kafe;
+
+/// <summary>
+/// Prescription for the shard abstraction without specifics of the persistence layer.
+/// </summary>
+/// <remarks>
+/// Shard representations in the persistence layer MUST implement this interface so that requirements can function
+/// without reference to the specific persistence implementation.
+/// </remarks>
+public interface IShard : IEntity
+{
+    LocalizedString Name { get; }
+
+    KafeObject Metadata { get; }
+
+    long FileLength { get; }
+
+    string MimeType { get; }
+
+    IReadOnlyDictionary<string, KafeObject> Variants { get; }
+}

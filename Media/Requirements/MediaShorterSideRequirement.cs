40aa657c5a62e6e39da0aca722b2b48ff4d0724c
Adam Štěpánek <xstepan1@fi.muni.cz>
2025-04-21 10:44:05 +0200

    Add MediaShorterSideRequirement

---
+++ Media/Requirements/MediaShorterSideRequirement.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading.Tasks;
+using Kafe.Core.Requirements;
+using Kafe.Media.Diagnostics;
+
+namespace Kafe.Media.Requirements;
+
+public record MediaShorterSideRequirement(
+    int Min
+) : IRequirement
+{
+    public static string Moniker { get; } = "media-shorter-side";
+}
+
+public class MediaShorterSideRequirementHandler : ShardRequirementHandlerBase<MediaShorterSideRequirement>
+{
+    public static readonly TimeSpan AcceptableDurationError = TimeSpan.FromSeconds(1);
+
+    public override ValueTask Handle(IShardRequirementContext<MediaShorterSideRequirement> context)
+    {
+        if (context.Shard.Metadata.Value is ImageInfo imageInfo)
+        {
+            if (Math.Min(imageInfo.Width, imageInfo.Height) < context.Requirement.Min)
+            {
+                context.Report(new MediaShorterSideTooShortDiagnostic(
+                    context.Shard.Name,
+                    context.Shard.Id,
+                    null,
+                    context.Requirement.Min
+                ));
+                return ValueTask.CompletedTask;
+            }
+        }
+
+        var mediaInfo = context.RequireMediaInfo();
+        if (mediaInfo is null)
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        foreach(var videoStream in mediaInfo.VideoStreams)
+        {
+            if (Math.Min(videoStream.Width, videoStream.Height) < context.Requirement.Min)
+            {
+                context.Report(new MediaShorterSideTooShortDiagnostic(
+                    context.Shard.Name,
+                    context.Shard.Id,
+                    null,
+                    context.Requirement.Min
+                ));
+                return ValueTask.CompletedTask;
+            }
+        }
+
+        return ValueTask.CompletedTask;
+    }
+}

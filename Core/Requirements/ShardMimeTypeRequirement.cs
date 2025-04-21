76bfbef822d167c462e045c37bff799d08c314d4
Adam Štěpánek <xstepan1@fi.muni.cz>
2025-04-21 10:57:34 +0200

    Add ShardMimeTypeRequirement

---
+++ Core/Requirements/ShardMimeTypeRequirement.cs
@@ -0,0 +1,41 @@
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Kafe.Core.Diagnostics;
+
+namespace Kafe.Core.Requirements;
+
+public record ShardMimeTypeRequirement(
+    ImmutableArray<string> Include,
+    ImmutableArray<string> Exclude
+) : IRequirement
+{
+    public static string Moniker { get; } = "shard-mime-type";
+}
+
+public class ShardMimeTypeRequirementHandler : ShardRequirementHandlerBase<ShardMimeTypeRequirement>
+{
+    public override ValueTask Handle(IShardRequirementContext<ShardMimeTypeRequirement> context)
+    {
+        if (context.Requirement.Include.IsDefaultOrEmpty && context.Requirement.Exclude.IsDefaultOrEmpty)
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        var allowedTypes = context.Requirement.Include.Except(context.Requirement.Exclude).ToImmutableArray().Sort();
+
+        if (!allowedTypes.Contains(context.Shard.MimeType))
+        {
+            context.Report(new ShardMimeTypeNotAllowedDiagnostic(
+                context.Shard.Id,
+                context.Shard.Name,
+                context.Shard.MimeType,
+                allowedTypes
+            ));
+            return ValueTask.CompletedTask;
+        }
+
+        return ValueTask.CompletedTask;
+    }
+}
+

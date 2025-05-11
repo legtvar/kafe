cf39ccedfeaafab097a9047c859b542f4118b86c
Adam Štěpánek <xstepan1@fi.muni.cz>
2025-05-11 14:27:28 +0200

    Fix ConfigureServices in PolygonsMod

--- Polygons/PolygonsMod.cs
+++ Polygons/PolygonsMod.cs
@@ -4,21 +4,23 @@ namespace Kafe.Polygons;
 
 public class PolygonsMod : IMod
 {
-    public static string Moniker {get;} = "polygons";
-    
-    public KafeType BlendShardType { get; private set; }
+    public static string Moniker { get; } = "polygons";
 
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddSingleton<BlendShardAnalyzer>();
-    }
+    public KafeType BlendShardType { get; private set; }
 
     public void Configure(ModContext context)
     {
+        ConfigureServices(context.Services);
+
         BlendShardType = context.AddShard<BlendInfo>(new()
         {
             Moniker = "blend",
             AnalyzerTypes = [typeof(BlendShardAnalyzer)]
         });
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<BlendShardAnalyzer>();
     }
 }

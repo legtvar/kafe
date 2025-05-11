bff27cbfdb0902a1497e4d59f283749fc257f6c2
Adam Štěpánek <xstepan1@fi.muni.cz>
2025-05-11 14:51:41 +0200

    Remove unnecesary RequirementContext extensions

--- Core/Requirements/RequirementContextExtensions.cs
+++ Core/Requirements/RequirementContextExtensions.cs
@@ -28,54 +28,6 @@ public static class RequirementContextExtensions
         return shard.Value;
     }
 
-    public static async Task<(IShard shard, KafeObject variant)?> RequireShardVariant(
-        this IRequirementContext<IRequirement> context,
-        string variant
-    )
-    {
-        var shard = await context.RequireShard();
-        if (shard is null)
-        {
-            return null;
-        }
-
-        if (!shard.Variants.TryGetValue(variant, out var variantObject))
-        {
-            context.Report(new MissingShardVariantDiagnostic(
-                ShardName: shard.Name,
-                ShardId: shard.Id,
-                Variant: Const.OriginalShardVariant
-            ));
-            return null;
-        }
-        return (shard, variantObject);
-    }
-
-    public static async Task<(IShard shard, TVariant variant)?> RequireShardVariant<TVariant>(
-        this IRequirementContext<IRequirement> context,
-        string variant
-    )
-    {
-        var pair = await context.RequireShardVariant(variant);
-        if (pair is null)
-        {
-            return null;
-        }
-
-        var (shard, variantObject) = pair.Value;
-
-        if (variantObject.Value is not TVariant typedVariant)
-        {
-            context.Report(new IncompatibleRequirementDiagnostic(
-                context.RequirementType,
-                variantObject.Type
-            ));
-            return null;
-        }
-
-        return (shard, typedVariant);
-    }
-
     public static async Task<(IShard shard, TMetadata metadata)?> RequireShardMetadata<TMetadata>(
         this IRequirementContext<IRequirement> context
     )

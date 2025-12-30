beea9a1b230fac6991a2fee9b7a293eda8bf4f62
Adam Štěpánek <xstepan1@fi.muni.cz>
2025-12-30 22:50:58 +0100

    Add a few tests for Err<T>

---
+++ Common/Tests/ErrTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Kafe.Common.Tests;
+
+public class ErrTests
+{
+    [Fact]
+    public void ErrOfInt_Default_ReturnDefault()
+    {
+        Err<int> err = default;
+        Assert.True(err.HasValue);
+        Assert.Equal(0, err.Value);
+    }
+
+    [Fact]
+    public void ErrOfObject_Default_ReturnGenericError()
+    {
+        Err<object> err = default;
+        Assert.False(err.HasValue);
+        Assert.True(err.Diagnostic.IsValid);
+        Assert.Equal(DiagnosticSeverity.Error, err.Diagnostic.Severity);
+        Assert.IsType<GenericErrorDiagnostic>(err.Diagnostic.Payload);
+    }
+
+    [Fact]
+    public void ErrOfInvalidHrib_Default_ReturnGenericError()
+    {
+        Err<Hrib> err = default;
+        Assert.False(err.HasValue);
+        Assert.True(err.Diagnostic.IsValid);
+        Assert.Equal(DiagnosticSeverity.Error, err.Diagnostic.Severity);
+        Assert.IsType<GenericErrorDiagnostic>(err.Diagnostic.Payload);
+    }
+}
+
+

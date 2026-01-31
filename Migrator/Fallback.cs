b0923f068136021729d55e76f7db6891fa15d2fa
Adam Štěpánek <xstepan1@fi.muni.cz>
2026-01-31 23:06:12 +0100

    Remove Migrator
    I may have to resurect it at some point from history, but right now I don't need it and I don't want to fix it.

--- Migrator/Fallback.cs
+++
@@ -1,9 +0,0 @@
-namespace Kafe.Migrator;
-
-public static class Fallback
-{
-    public const string ProjectName = "Project {}";
-    public const string ProjectGroupName = "Project Group {}";
-    public const string AuthorName = "Author {}";
-    public const string PlaylistName = "Playlist {}";
-}
